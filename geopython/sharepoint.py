38fdf3bc3533c17d43d6e08882c41f770cf970dc
John Sandall <2884159+john-sandall@users.noreply.github.com>
2023-03-05 15:56:17 +0100

    Add GitHub Action for a on-demand triggered workflow

--- geopython/sharepoint.py
+++ geopython/sharepoint.py
@@ -1,13 +1,19 @@
 """Use Python to access Sharepoint.
 
-Usage:
-    $ python -m geopython.sharepoint
+Usage: python -m geopython.sharepoint [OPTIONS] SHAREPOINT_FILENAME
+
+Examples:
+    python -m geopython.sharepoint
+    python -m geopython.sharepoint "Input.xlsx"
+    python -m geopython.sharepoint --help
 """
 import os
 from datetime import datetime
 from pathlib import Path
+from typing import Optional
 
 import pandas as pd
+import typer
 from loguru import logger as log
 from shareplum import Office365, Site
 from shareplum.site import Version
@@ -23,26 +29,19 @@ def authenticate(site_url, site_name, username, password):
     return Site(f"{site_url}/sites/{site_name}/", version=Version.v365, authcookie=authcookie)
 
 
-def download_input_file(folder):
+def download_input_file(folder, filename: Optional[str] = "Input.xlsx"):
     # We could iterate through the files for any files that look like "Input*.xlsx" and process them
     # all one-by-one. For now, we will not do this.
     # files = demo_folder.files  # noqa: E800
     # filename = files[0]["Name"]  # noqa: E800
 
     # Assume the file is called "Input.xlsx" and download this
-    filename = "Input.xlsx"
     file_contents = folder.get_file(filename)
     filepath = DATA_DIR / "interim" / filename
     with open(filepath, "wb") as f:
         f.write(file_contents)
 
     return filepath
-
-
-def upload_file(filepath, sharepoint_folder):
-    with open(filepath, "rb") as f:
-        file_contents = f.read()
-        sharepoint_folder.upload_file(file_contents, filepath.name)
 
 
 def process_xlsx(filepath):
@@ -60,22 +59,32 @@ def process_xlsx(filepath):
 
     # Export to local file
     timestamp = datetime.now().strftime("%Y-%m-%d_%H%M")
-    output_filepath = DATA_DIR / "processed" / f"Output__{timestamp}.xlsx"
+    filename = filepath.name.replace(".xlsx", "")
+    output_filepath = DATA_DIR / "processed" / f"{filename}__{timestamp}.xlsx"
     df.to_excel(output_filepath, index=False)
 
     return output_filepath
 
 
-def main(site_url, site_name, username, password):
+def upload_file(filepath, sharepoint_folder):
+    with open(filepath, "rb") as f:
+        file_contents = f.read()
+        sharepoint_folder.upload_file(file_contents, filepath.name)
+
+
+def main(sharepoint_filename: Optional[str]):
+    site_url = os.environ["SHAREPOINT_SITE_URL"]
+    site_name = os.environ["SHAREPOINT_SITE_NAME"]
+    username = os.environ["SHAREPOINT_USERNAME"]
+    password = os.environ["SHAREPOINT_PASSWORD"]
+
     # Authenticate with Sharepoint site
     site = authenticate(site_url, site_name, username, password)
     log.info(f"Authenticated with Sharepoint site: {site_url}")
 
-    # Locate files in the Demo folder; we will assume the Input file is always called "Input.xlsx"
-    demo_folder = site.Folder("Shared Documents/Demo")
-
-    # Download a file called "Input.xlsx" in the Demo folder of Sharepoint
-    downloaded_file = download_input_file(folder=demo_folder)
+    # Download the specified file from the Input folder of Sharepoint
+    input_folder = site.Folder("Shared Documents/Input")
+    downloaded_file = download_input_file(folder=input_folder, filename=sharepoint_filename)
     log.info(f"Downloaded file: {downloaded_file}")
 
     # Process the file (concatenate names, format date, geocode the location)
@@ -83,14 +92,10 @@ def main(site_url, site_name, username, password):
     log.info(f"Saved processed file into: {processed_file}")
 
     # Upload local file to Sharepoint "Demo" folder
-    upload_file(processed_file, demo_folder)
+    output_folder = site.Folder("Shared Documents/Output")
+    upload_file(processed_file, output_folder)
     log.opt(ansi=True).info("✅ <green>Successfully uploaded file back to Sharepoint</green> 🎉")
 
 
 if __name__ == "__main__":
-    main(
-        site_url=os.environ["SHAREPOINT_SITE_URL"],
-        site_name=os.environ["SHAREPOINT_SITE_NAME"],
-        username=os.environ["SHAREPOINT_USERNAME"],
-        password=os.environ["SHAREPOINT_PASSWORD"],
-    )
+    typer.run(main)
